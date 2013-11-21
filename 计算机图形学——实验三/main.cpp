7229a9e9c08773c3c440deae8b4550d3540639fe
修宇亮 <Personals@osamusakaisuketekiMacBook-Pro.local>
2013-11-22 00:37:12 +0800

    Initial Commit

---
+++ 计算机图形学——实验三/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  计算机图形学——实验三
+//
+//  Created by 修宇亮 on 13-11-22.
+//  Copyright (c) 2013年 修宇亮. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
